f0fa3e0c8ba61c05072788b693b9c155ce218b21
Nick <oxdontwearsox@gmail.com>
2017-03-14 00:25:59 -0400

    Added screenshots, completed task 4

---
+++ Task4/app/src/main/java/com/example/nick/werlep4task4/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.nick.werlep4task4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void compute_onClick(View view){
+        Intent i = new Intent(MainActivity.this, ResultActivity.class);
+        Integer n = Integer.parseInt(((EditText)findViewById(R.id.editText2)).getText().toString());
+        i.putExtra("n",n);
+
+        startActivity(i);
+    }
+}
